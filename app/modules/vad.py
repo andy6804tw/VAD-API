1810845ea7238e128489c4623984362f61449990
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-06-26 11:47:48 +0800

    modify

--- app/modules/vad.py
+++ app/modules/vad.py
@@ -9,7 +9,7 @@ import numpy as np
 
 model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
                               model='silero_vad',
-                              force_reload=True)
+                              force_reload=False)
 
 (get_speech_timestamps,
  save_audio,
