5e2b50b7fb59f6be5eff40943f633eb09e5ff3be
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-06-26 10:54:24 +0800

    close debug mode

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def index():
 
 if __name__ == '__main__':
     print(app.url_map)
-    app.run(host='0.0.0.0', port=config.PORT, debug=True)
+    app.run(host='0.0.0.0', port=config.PORT, debug=False)
